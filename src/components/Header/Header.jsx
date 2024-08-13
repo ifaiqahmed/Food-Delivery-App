d63633ef826333cb43bd6171c7c0377da866322a
Denis Murphy <faiqaws1234@gmail.com>
2024-08-13 23:14:22 +0500

    Second Commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,7 @@ const Header = () => {
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
       <p>Choose from a diverse menu featuring a delectable array of</p>
+      <button>View Menu</button>
       </div>
     </div>
   )
